f129e060d69b2b2aaf434c450ff1094f2afe7caf
redrover9 <grace.thompson97@gmail.com>
2020-08-29 19:36:25 -0700

    Add files via upload

---
+++ calendars.php
@@ -0,0 +1,33 @@
+<?php
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://www.sefaria.org/api/calendars/?timezone=America/Vancouver&custom=ashkenazi");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+$data = curl_exec($ch);
+
+$array = json_decode($data, true);                    
+$parashah = $array['calendar_items'][0]['displayValue']['en'];
+$aliyot = $array['calendar_items'][0]['extraDetails']['aliyot'];
+$haftarah = $array['calendar_items'][1]['displayValue']['en'];
+
+echo "<h2>Parashah</h2>";
+echo $parashah;
+echo "<br>";
+
+echo "<h2>Aliyot</h2>";
+
+$day = 'Sun '; 
+
+foreach ($aliyot as $i) {
+    if ($day != 'Sun') {
+        echo $day . " " . $i;
+    }
+        $day = date('D',strtotime($day.'+1 day'));
+        echo '<br>';
+}
+echo "<br>";
+
+echo "<h2>Haftarah</h2>";
+echo $haftarah;
+echo "<br>";
