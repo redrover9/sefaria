03ff43daf71ba19d418e93c24b57b5af2ba5ebd9
redrover9 <grace.thompson97@gmail.com>
2020-11-30 09:48:58 -0800

    Add Noach and Lech Lecha

--- torahApp.php
+++ torahApp.php
@@ -23,6 +23,48 @@ elseif ($parasha == 'Bereshit' && $aliyah == '6') {
 elseif ($parasha == 'Bereshit' && $aliyah == '7') {
 	$verses = 'Genesis.5.25-6.8';
 }
+elseif ($parasha == 'Noach' && $aliyah == '1') {
+	$verses = 'Genesis.6.9-22';
+}
+elseif ($parasha == 'Noach' && $aliyah == '2') {
+	$verses = 'Genesis.7.1-16';
+}
+elseif ($parasha == 'Noach' && $aliyah == '3') {
+	$verses = 'Genesis.7.17-8.14';
+}
+elseif ($parasha == 'Noach' && $aliyah == '4') {
+	$verses = 'Genesis.8.15-9.7';
+}
+elseif ($parasha == 'Noach' && $aliyah == '5') {
+	$verses = 'Genesis.9.8-17';
+}
+elseif ($parasha == 'Noach' && $aliyah == '6') {
+	$verses = 'Genesis.9.18-10.32';
+}
+elseif ($parasha == 'Noach' && $aliyah == '7') {
+	$verses = 'Genesis.11.1-32';
+}
+elseif ($parasha == 'Lech Lecha' && $aliyah == '1') {
+	$verses = 'Genesis.12.1-13';
+}
+elseif ($parasha == 'Lech Lecha' && $aliyah == '2') {
+	$verses = 'Genesis.12.14-13.4';
+}
+elseif ($parasha == 'Lech Lecha' && $aliyah == '3') {
+	$verses = 'Genesis.13.5-18';
+}
+elseif ($parasha == 'Lech Lecha' && $aliyah == '4') {
+	$verses = 'Genesis.14.1-20';
+}
+elseif ($parasha == 'Lech Lecha' && $aliyah == '5') {
+	$verses = 'Genesis.14.21-15.6';
+}
+elseif ($parasha == 'Lech Lecha' && $aliyah == '6') {
+	$verses = 'Genesis.15.7-17.6';
+}
+elseif ($parasha == 'Lech Lecha' && $aliyah == '7') {
+	$verses = 'Genesis.17.7-27';
+}
 echo $parasha;
 echo '<br>';
 echo $aliyah;
